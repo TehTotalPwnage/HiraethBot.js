5699ba9205d444b055404e4b950469a5455b7d8a
Michael Nguyen <tehtotalpwnage@gmail.com>
2016-09-24 15:37:45 -0700

    Begin port to discord.js v9

--- libs/Discord.js
+++ libs/Discord.js
@@ -2,15 +2,10 @@ const Discord = require("discord.js");
 
 const Config = require('./Config');
 
-var DiscordBot = new Discord.Client({
-	autoReconnect: true
-});
-DiscordBot.loginWithToken(Config.discord.token).then(token => console.log("Logged into Discord with token : " + token));
-const postMessage = function(message) {
-	DiscordBot.deleteMessage(message, { wait:10000 });
-};
+var DiscordBot = new Discord.Client();
+DiscordBot.login(Config.discord.token).then(token =>
+	console.log("Logged into Discord with token : " + token));
 module.exports.Bot = DiscordBot;
-module.exports.postMessage = postMessage;
 
 const HiraethBot = require('./HiraethBot');
 
@@ -21,7 +16,7 @@ const argsFalse = {
 	"accept": function(message) { Duel.accept(message); },
 	"dab": function(message) { DiscordBot.sendMessage(message.channel, {file:{file: __dirname + "/../assets/images/60_21i.gif" }}); },
 	"fiftyfifty": function(message) { HiraethBot.Reddit.getFiftyfifty(result => DiscordBot.sendMessage(message.channel, "**[" + message.author + "] [!fiftyfifty]** " + result)); },
-	"ping": function(message) { DiscordBot.reply(message, "Pong!").then(postMessage); },
+	"ping": function(message) { DiscordBot.reply(message, "Pong!").then(message.delete(10000)); },
 	"rigduel": function(message) { Duel.rigduel(message); },
 	"shutdown": function(message) { shutdown(message); }
 };
@@ -36,34 +31,33 @@ const argsTrue = {
 	"polandball": function(message, value) { HiraethBot.Reddit.getPolandball(value, result=> DiscordBot.sendMessage(message.channel, "**[" + message.author + "] [!polandball]** " + result)); }
 };
 
-DiscordBot.on("ready", function () {
-	DiscordBot.joinVoiceChannel(Config.discord.voicechannel)
-		.then(connection => {
-			connection.playFile(__dirname + '/../assets/audio/poi.mp3', {volume: 0.25});
-			console.log("Connected to voice channel " + connection.voiceChannel.name);
-		}).catch(console.log);
-	DiscordBot.sendMessage(Config.discord.relaychannel.toString(), "**Now poi-ing!**");
+DiscordBot.on("ready", () => {
+	// DiscordBot.joinVoiceChannel(Config.discord.voicechannel)
+	// 	.then(connection => {
+	// 		connection.playFile(__dirname + '/../assets/audio/poi.mp3', {volume: 0.25});
+	// 		console.log("Connected to voice channel " + connection.voiceChannel.name);
+	// 	}).catch(console.log);
+	// DiscordBot.sendMessage(Config.discord.relaychannel.toString(), "**Now poi-ing!**");
+	console.log("Now serving " + DiscordBot.guilds.length + " servers.");
 });
 
-DiscordBot.on("message", function (message) {
+DiscordBot.on("message", (message) => {
 	console.log("[CHAT]	[DISCORD] [" + message.author.name + "]: " + message.content);
 	if (message.content.startsWith("!")) {
-		var args = message.content.indexOf(" ");
+		var args = message.content.split(" ");
 		var param, value;
-		if (args === -1) {
-			param = message.content.substr(1);
-		} else {
-			param = message.content.substr(1, message.content.indexOf(" ") - 1);
-			value = message.content.substr(message.content.indexOf(" ") + 1);
+		param = args[0].substr(1);
+		if (args.length !== 1) {
+			value = args[1];
 		}
 		if (!(param in argsFalse || param in argsTrue)) {
-			DiscordBot.reply(message, "Unrecognized command. Run !help for a list of commands.").then(postMessage);
+			DiscordBot.reply(message, "Unrecognized command. Run !help for a list of commands.").then(message.delete(10000));
 		} else if ((value) && param in argsTrue) {
 			argsTrue[param](message, value);
 		} else if ((!value) && param in argsFalse ) {
 			argsFalse[param](message);
 		} else {
-			DiscordBot.reply(message, "Incorrect number of arguments!").then(postMessage);
+			DiscordBot.reply(message, "Incorrect number of arguments!").then(message.delete(10000));
 		}
 		DiscordBot.deleteMessage(message, { wait:5000 });
 	} else if (HiraethBot.Plug.getSelf().role === 3 && message.author !== DiscordBot.user && message.channel === DiscordBot.channels.get("id", Config.discord.relaychannel)) {
